1dd87b4475c48eec892f3a3564f537b246ccc60f
Joker9944 <9194199+Joker9944@users.noreply.github.com>
2024-09-03 02:44:45 +0200

    Adjust oidc config for owncloud HelmRelease

--- apps/cloud-apps/owncloud/oidc.config.php
+++ apps/cloud-apps/owncloud/oidc.config.php
@@ -7,19 +7,11 @@ $CONFIG = [
 		'client-secret' => "{{ environ('OWNCLOUD_OIDC_CLIENTSECRET') }}",
 		'loginButtonName' => 'Login with OpenID',
 		'mode' => 'userid',
+		'search-attribute' => 'preferred_username',
 		'auto-provision' => [
-			// explicit enable the auto provisioning mode
 			'enabled' => true,
-			// enable the user info auto-update mode
 			'update' => [ 'enabled' => true, ],
-			// documentation about standard claims: https://openid.net/specs/openid-connect-core-1_0.html#StandardClaims
-			// only relevant in userid mode,  defines the claim which holds the email of the user
-			'email-claim' => 'email',
-			// defines the claim which holds the display name of the user
 			'display-name-claim' => 'preferred_username',
-			// defines the claim which holds the picture of the user - must be a URL
-			// 'picture-claim' => 'picture',
-			// defines a list of groups to which the newly created user will be added automatically
 			'groups' => [ 'admin' ],
 		],
 	],
